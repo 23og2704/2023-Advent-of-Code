8359a4afd0aee0061055524320fd3184ad898480
wxterfall <23og2704@barton.ac.uk>
2023-12-02 20:08:01 +0000

    [+] Part 2 of day 2 added and it definetly works 100% [+] edits to part 1 so it looks cooler

--- Day 2 - Part 2/Program.cs
+++ Day 2 - Part 2/Program.cs
@@ -12,36 +12,32 @@ namespace December_2nd
             bool result = Int32.TryParse(num, out number);
             return result;
         }
-        static string IDFinder(string line)
+        static int CalculatePower(int[] cubes)
         {
-            string[] ID = line.Split(':');
-            string[] num = ID[0].Split(' ');
-            return num[1];
+            return cubes[0] * cubes[1] * cubes[2];
         }
+
         static void Main(string[] args)
         {
             List<string> lines = new List<string>(File.ReadAllLines("games.txt"));
-            List<int> IDs = new List<int>();
             int result = 0;
+
             foreach (string line in lines)
             {
-                int id = int.Parse(IDFinder(line));
-                Console.WriteLine(line);
                 int[] numofcol = { 0, 0, 0 };
                 string[] splitstring = line.Split(':');
                 string[] sections = splitstring[1].Split(';');
+
                 foreach (string section in sections)
                 {
-                    Console.WriteLine($"Section: {section}");
                     string[] parts = section.Split(',');
                     foreach (string part in parts)
                     {
-                        Console.WriteLine($"Part: {part}");
                         string num = "";
                         string[] keys = part.Split(' ');
+
                         foreach (string key in keys)
                         {
-
                             if (isNum(key) == true)
                             {
                                 num += key;
@@ -60,23 +56,21 @@ namespace December_2nd
                                         if (int.Parse(num) > numofcol[2]) numofcol[2] = int.Parse(num);
                                         break;
                                 }
+                                num = "";
                             }
                         }
-
                     }
-                    Console.WriteLine($"Red: {numofcol[0]}, Green: {numofcol[1]}, Blue: {numofcol[2]}");
                 }
-                if (numofcol[0] <= 12 && numofcol[1] <= 13 && numofcol[2] <= 14)
-                {
-                    result += id;
-                    IDs.Add(id);
-                }
-                Console.Write($"Id's Possible: ");
-                foreach (int val in IDs)
-                    Console.Write($"{val}, ");
-                Console.WriteLine($"Result: {result}");
 
+                // Console.WriteLine($"Red: {numofcol[0]}, Green: {numofcol[1]}, Blue: {numofcol[2]}");
+
+                int power = CalculatePower(numofcol);
+                result += power;
+
+                Console.WriteLine($"Power: {power}");
             }
+
+            Console.WriteLine($"Sum of powers: {result}");
             Console.ReadKey();
         }
     }
