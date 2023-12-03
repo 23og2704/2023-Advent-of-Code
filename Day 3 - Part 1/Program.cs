8c9207476a01d6337013a03e87d944036e98202e
wxterfall <23og2704@barton.ac.uk>
2023-12-03 16:36:43 +0000

    [+] added maybe working day 3 part 1 code i think idk

---
+++ Day 3 - Part 1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+
+namespace December_3rd
+{
+    internal class Program
+    {
+        static int toInt(char c)
+        {
+            return (int)(c - '0');
+        }
+
+        static bool isSymbol(char c)
+        {
+            return !char.IsNumber(c) && !(c == '.');
+        }
+
+        static bool isValid(int x, int y, int numCols, int numRows)
+        {
+            return x >= 0 && x < numCols && y >= 0 && y < numRows;
+        }
+
+        static bool findPartNums(string[] lines, int i, int j, int place, int numCols, int numRows)
+        {
+            for (int y = i - 1; y <= i + 1; y++)
+            {
+                for (int x = j - 1; x <= j + place; x++)
+                {
+                    if (isValid(x, y, numCols, numRows) && isSymbol(lines[y][x]))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        static void Main()
+        {
+            int total = 0;
+
+            string[] lines = File.ReadAllLines("text.txt");
+            int numberRows = lines.Length;
+            int numberCols = lines[0].Length;
+
+            for (int i = 0; i < numberRows; i++)
+            {
+                for (int j = 0; j < numberCols; j++)
+                {
+                    if (char.IsNumber(lines[i][j]))
+                    {
+                        int num = 0;
+                        int place = 0;
+
+                        while (j + place < numberCols && char.IsNumber(lines[i][j + place]))
+                        {
+                            num = num * 10 + toInt(lines[i][j + place]);
+                            place++;
+                        }
+
+                        bool valid = findPartNums(lines, i, j, place, numberCols, numberRows);
+
+                        if (valid)
+                        {
+                            total += num;
+                        }
+
+                        j += place;
+                    }
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+
+
+    }
+
+}
